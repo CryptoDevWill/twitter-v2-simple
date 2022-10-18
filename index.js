d0e2751daf8b1d1e9ab6abb8bcd569ce40c10414
Will <williamdsanders05@gmail.com>
2022-10-18 19:21:23 +0000

    Git ignore

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
+import { TwitterApi } from 'twitter-api-v2';
 
+
+export default class TwitterV2Simple {
+        constructor(config){
+            this.client = new TwitterApi({
+                appKey: config.appKey,
+                appSecret: config.appSecret,
+                accessToken: config.accessToken,
+                accessSecret: config.accessSecret
+            })
+        }
+        async getTweet(id){
+            const response = await this.client.v2.singleTweet(id, {
+                "tweet.fields": [
+                    "author_id"
+                ],
+                expansions: [
+                  'entities.mentions.username',
+                  'in_reply_to_user_id',
+                ],
+              });
+
+              console.log(response)
+        }
+
+
+
+
+
+}
